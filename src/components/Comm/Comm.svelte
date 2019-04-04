88c76b434990aaa19ba52e5bc07bbe6d8bf76716
Stephen Sprinkle <stephen@sprinkle.io>
2019-04-04 13:27:35 -0700

    added cleanup script + scaling viewport improvements to site

---
+++ src/components/Comm/Comm.svelte
@@ -0,0 +1,50 @@
+<div class="comm">
+  <a href="https://twitter.com/stephensprinkle">
+    <i class="fab fa-twitter"></i>
+  </a>
+  <a href="https://www.linkedin.com/in/stephensprinkle">
+    <i class="fab fa-linkedin-in"></i>
+  </a>
+  <a href="https://github.com/stephensprinkle">
+    <i class="fab fa-github"></i>
+  </a>
+  <a href="mailto:hi@sprinkle.io">
+    <i class="far fa-envelope"></i>
+  </a>
+</div>
+
+<style>
+  .comm {
+    margin: 0.8em;
+  }
+
+  .comm a {
+    text-decoration: none;
+  }
+
+  .comm a i {
+    color: #898989;
+    transition: color 0.2s ease-in-out;
+  }
+
+  .comm a:hover i.fa-twitter {
+    color: #00aced;
+  }
+
+  .comm a:hover i.fa-linkedin-in {
+    color: #0077B5;
+  }
+
+  .comm a:hover i.fa-github {
+    color: #333;
+  }
+
+  .comm a:hover i.fa-envelope {
+    color: #4285F4;
+  }
+
+  .comm i {
+    font-size: 2.8em;
+    margin: 0 0.2em;
+  }
+</style>
--- src/components/Main/Main.svelte
+++ src/components/Main/Main.svelte
@@ -3,20 +3,7 @@
     <div class="nucleus" >
       <span>SS</span>
     </div>
-    <div class="comm">
-      <a href="https://twitter.com/stephensprinkle">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a href="https://www.linkedin.com/in/stephensprinkle">
-        <i class="fab fa-linkedin-in"></i>
-      </a>
-      <a href="https://github.com/stephensprinkle">
-        <i class="fab fa-github"></i>
-      </a>
-      <a href="mailto:hi@sprinkle.io">
-        <i class="far fa-envelope"></i>
-      </a>
-    </div>
+    <Comm />
   </section>
   <Footer />
 </main>
@@ -54,47 +41,15 @@
     display: block;
     font-size: 2.6em;
   }
-
-  .comm {
-    margin: 0.8em;
-  }
-
-  .comm a {
-    text-decoration: none;
-  }
-
-  .comm a i {
-    color: #898989;
-    transition: color 0.2s ease-in-out;
-  }
-
-  .comm a:hover i.fa-twitter {
-    color: #00aced;
-  }
-
-  .comm a:hover i.fa-linkedin-in {
-    color: #0077B5;
-  }
-
-  .comm a:hover i.fa-github {
-    color: #333;
-  }
-
-  .comm a:hover i.fa-envelope {
-    color: #4285F4;
-  }
-
-  .comm i {
-    font-size: 2.8em;
-    margin: 0 0.2em;
-  }
 </style>
 
 <script>
+import Comm from '../Comm/Comm.svelte';
 import Footer from '../Footer/Footer.svelte';
 
 export default {
   components: {
+    Comm,
     Footer
   }
 }
